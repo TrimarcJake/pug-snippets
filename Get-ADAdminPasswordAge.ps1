204c51779ee01ff833caaa8f35ea5d3ceb62344a
TrimarcJake <jakehildreth@trimarcsecurity.com>
2024-04-02 20:24:02 -0500

    Clean up, pick up, put away.

--- Get-ADAdminPasswordAge.ps1
+++ Get-ADAdminPasswordAge.ps1
@@ -1,41 +1,40 @@
 $Forest = Get-ADForest
-$ForestGC = $(Get-ADDomainController -Discover -Service GlobalCatalog -ForceDiscover | Select-Object -ExpandProperty Hostname) + ":3268"
 $Domains = $Forest.Domains
-$AdaMemberDNs = @()
-$AdaMemberNamesAndPasswords = @()
+$DNs = @()
+$PrincipalInfo = @()
 
 $Domains | ForEach-Object {
     $DomainSid = (Get-ADDomain -Server $_).DomainSID.Value
+    
     $KrbtgtSid = $DomainSid + "-502"
+    $DNs += (Get-ADUser -Server $_ -Identity $KrbtgtSid).distinguishedName
+
     $BASid = 'S-1-5-32-544'
     $DASid = $DomainSid + '-512'
     $EASid = $DomainSid + '-519'
     $SASid = $DomainSid + '-518'
-    # $ADAGroupSids = @($BASid, $DASid, $EASid, $SASid)
-    $Krbtgt = Get-ADUser -Server $_ -Identity $KrbtgtSid -Properties PasswordLastSet
-
     foreach ($sid in @($BASid, $DASid, $EASid, $SASid)) {
         try {
-            $ADAMemberDNs += (Get-ADGroupMember -Server $_ -Identity $sid -Recursive).distinguishedName
-            $ADAMemberDNs += (Get-ADGroup -Server $_ -Identity $sid).Members
+            $DNs += (Get-ADGroupMember -Server $_ -Identity $sid -Recursive).distinguishedName
+            $DNs += (Get-ADGroup -Server $_ -Identity $sid).Members
         } catch {}
     }
 
-    $ADAMemberDNs = $ADAMemberDNs | Sort-Object -Unique
+    $DNs = $DNs | Sort-Object -Unique
     
-    foreach ($dn in $ADAMemberDNs) {
+    foreach ($dn in $DNs) {
         $Domain = $_
         try {
-            $ADAMemberNamesAndPasswords += Get-ADUser -Server $Domain -Identity $dn -Properties PasswordLastSet | Select-Object {$Domain}, Name, objectClass, PasswordLastSet
+            $PrincipalInfo += Get-ADUser -Server $Domain -Identity $dn -Properties PasswordLastSet | Select-Object {$Domain}, Name, objectClass, PasswordLastSet
         } catch {}
         try {
-            $ADAMemberNamesAndPasswords += Get-ADComputer -Server $Domain -Identity $dn -Properties PasswordLastSet | Select-Object {$Domain}, Name, objectClass, PasswordLastSet
+            $PrincipalInfo += Get-ADComputer -Server $Domain -Identity $dn -Properties PasswordLastSet | Select-Object {$Domain}, Name, objectClass, PasswordLastSet
         } catch {}
         try {
-            $ADAMemberNamesAndPasswords += Get-ADServiceAccount -Server $Domain -Identity $dn -Properties PasswordLastSet | Select-Object {$Domain}, Name, objectClass, PasswordLastSet
+            $PrincipalInfo += Get-ADServiceAccount -Server $Domain -Identity $dn -Properties PasswordLastSet | Select-Object {$Domain}, Name, objectClass, PasswordLastSet
         } catch {}
     }
 }
 
-$AdaMemberNamesAndPasswords = $AdaMemberNamesAndPasswords | Sort-Object -Property Name
-$AdaMemberNamesAndPasswords
+$PrincipalInfo = $PrincipalInfo | Sort-Object -Property PasswordLastSet
+$PrincipalInfo | Format-Table -AutoSize
