de654c764eb5b75cb55436768b2635d856de8f0d
TrimarcJake <jakehildreth@trimarcsecurity.com>
2024-04-07 06:47:31 -0500

    Updated XPath Filters

--- code/Get-KereberosEncryption.ps1
+++ code/Get-KereberosEncryption.ps1
@@ -1,1 +1,34 @@
-Get-WinEvent -FilterHashTable @{ LogName = 'Security'; ID = 4768 }
+$ADAdminName = 'BTPC02$'
+$filter = @"
+    *[EventData
+        [Data
+            [@Name='TargetUserName']
+            and
+            (Data='$ADAdminName')
+        ]
+    ]
+    [System
+        [(EventID=4768 or EventID=4769)]
+    ]
+"@
+Get-WinEvent -FilterXPath $filter -LogName Security
+
+$ADAdminName = 'BTPC02$'
+$filter = @"
+    *[EventData
+        [Data
+            [@Name='TargetUserName']
+            and
+            (Data='$ADAdminName')
+        ]
+        [Data
+            [@Name='EncryptionType']
+            and
+            (Data=0x12)
+        ]
+    ]
+    [System
+        [(EventID=4768 or EventID=4769)]
+    ]
+"@
+Get-WinEvent -FilterXPath $filter -LogName Security
--- code/Get-NTLMLogons.ps1
+++ code/Get-NTLMLogons.ps1
@@ -1,3 +1,19 @@
 $ADAdminName = 'jhda'
-Get-WinEvent -FilterXPath "*[EventData[Data[@Name='AuthenticationPackageName'] and (Data='NTLM'
-or Data='Negotiate')][Data[@Name='TargetUserName'] and (Data='jhda'')]][System[(EventID=4624 or EventID=4625)]]" -LogName Security
+$filter = @"
+    *[EventData
+        [Data
+            [@Name='AuthenticationPackageName']
+            and
+            (Data='NTLM' or Data='Negotiate')
+        ]
+        [Data
+            [@Name='TargetUserName']
+            and
+            (Data='$ADAdminName')
+        ]
+    ]
+    [System
+        [(EventID=4624 or EventID=4625)]
+    ]
+"@
+Get-WinEvent -FilterXPath $filter -LogName Security
